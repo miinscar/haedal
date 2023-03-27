f884bd74027250d79ec2ba698aa0cc6cb2b63669
miinscar <128962867+miinscar@users.noreply.github.com>
2023-03-27 19:35:46 +0900

    Update hw_0.py

--- hw_0.py
+++ hw_0.py
@@ -2,7 +2,7 @@
 def Insertion_sort(arr):
     for i in range(1, len(arr)):
       a = arr[i]
-      j = i -1
+      j = i - 1
       while j >= 0 and arr[j] > a:
           arr[j+1] = arr[j]
           j -= 1
