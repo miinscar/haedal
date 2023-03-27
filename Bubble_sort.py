0d042cf63982c319bb222934fe3d2a199d44bbb6
miinscar <128962867+miinscar@users.noreply.github.com>
2023-03-27 20:42:42 +0900

    Delete Bubble_sort.py

--- Bubble_sort.py
+++
@@ -1,7 +0,0 @@
-def Bubble_sort(arr):
-    for i in range(len(arr) - 1):
-        for j in range(len(arr) - i - 1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                
-    return arr
